795d73719a11f6e5f877b146a1a7aef1ca7e9ecf
DennisDaddy <softteez95@gmail.com>
2016-11-30 18:02:25 +0300

    add sections

--- view_posts.php
+++ view_posts.php
@@ -140,6 +140,20 @@
           </div>
       </aside>
       <!--sidebar end-->
+      <!--main content start-->
+      <section id="main-content">
+      <section class="wrapper">            
+              <!--overview start-->
+        <div class="row">
+        <div class="col-lg-12">
+          <h3 class="page-header"><i class="fa fa-laptop"></i> Dashboard</h3>
+          <ol class="breadcrumb">
+            <li><i class="fa fa-home"></i><a href="index.html">Home</a></li>
+            <li><i class="fa fa-laptop"></i>Dashboard</li> 
+            </ol>               
+
+
+
 <table class="table">
 <tr>
 	<td colspan="8" align="center" bgcolor="white"><h1>View All Posts</h1></td>
@@ -186,6 +200,14 @@ while ($row=mysqli_fetch_array($run)) {
 <?php } ?>
 	
 </table>
+
+</div>
+      </div>
+          
+      </section>
+      <!--main content end-->
+  </section>
+  <!-- container section start -->
 
 <!-- javascripts -->
     <script src="js/jquery.js"></script>
