d180a74a2907c3eb451ccc384557f7f1a7371a11
DennisDaddy <softteez95@gmail.com>
2016-12-01 08:16:48 +0300

    add change html file

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
             </div>
 
             <!--logo start-->
-            <a href="index.html" class="logo">Admin<span class="lite">Area</span></a>
+            <a href="index.php" class="logo">Admin<span class="lite">Area</span></a>
             <!--logo end-->
 
             <div class="nav search-row" id="top_menu">
@@ -75,7 +75,7 @@
                             <div class="log-arrow-up"></div>                          
                     
                             <li>
-                                <a href="login.html"><i class="icon_key_alt"></i> Log Out</a>
+                                <a href="login.php"><i class="icon_key_alt"></i> Log Out</a>
                             </li>
                             
                         </ul>
